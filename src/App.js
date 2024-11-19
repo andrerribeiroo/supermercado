b270e635165390f0a9a6925f8eeb2cef4a6ea0cb
Andre Ribeiro <168545471+andrerribeiroo@users.noreply.github.com>
2024-11-19 18:34:01 -0300

    ADD

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
 import Home from './pages/Home';
 
+// Importação de componentes
+import NavBarra from "./components/NavBarra";
+
+//Importação do gerenciador de rotas
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Login />
-      {/* <Home/> */}
-
+      <BrowserRouter>
+      <NavBarra />
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/login" element={<Login />}/>  
+          <Route path="/home" element={<Home />}/>        
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
